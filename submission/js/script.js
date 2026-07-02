edc97d1166e125751db7ab5c098a140a1c6863d8
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-07-02 19:40:31 +0300

    banner reveal

--- submission/js/script.js
+++ submission/js/script.js
@@ -6,20 +6,6 @@ bannerImage.addEventListener("click",event =>{
     bannerInfo.classList.toggle("bannerInfo");
 } );
 
-let themeToggle = document.querySelector("#themeToggle");
-
-themeToggle.addEventListener("click", event=>{
-document.body.classList.toggle("dark-mode");
-if(document.body.classList.toggle("dark-mode")){
-    localStorage.setItem("theme", "dark");
-}else{
-    localStorage.setItem("theme", "light");
-}
-});
-let savedTheme = localStorage.getItem("theme-btn");
-if(savedTheme === "dark"){
-    document.body.classList.add("dark-mode");
-}
 
 // WishList add and Remove
  
