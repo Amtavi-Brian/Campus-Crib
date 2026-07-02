d8187d9e504ffff7181f57324a0d0dbe239e7bc6
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-07-02 17:58:26 +0300

    Dynamically add and delete items

--- submission/js/script.js
+++ submission/js/script.js
@@ -20,3 +20,26 @@ let savedTheme = localStorage.getItem("theme-btn");
 if(savedTheme === "dark"){
     document.body.classList.add("dark-mode");
 }
+
+// WishList add and Remove
+ 
+let wishListInput = document.querySelector("#wishlistInput");
+let addWishList = document.querySelector("#addWishlist");
+let wishlist = document.querySelector("#wishlist");
+addWishList.addEventListener("click",event =>{
+    event.preventDefault();
+let item = wishListInput.value.trim();
+if(item !== ""){
+    let li = document.createElement("li");
+    li.textContent = item + " ";
+
+    let removebtn = document.createElement("button");
+    removebtn.textContent = "Remove";
+    removebtn.addEventListener("click", event =>{
+        li.remove();
+    });
+    li.appendChild(removebtn);
+    wishlist.appendChild(li);   
+
+}
+});
