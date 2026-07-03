81df531c0d1b176a7e88acf626edc2b03f9a8749
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-07-03 08:37:24 +0300

    LocalStrorage for theme toggle

--- submission/js/script.js
+++ submission/js/script.js
@@ -5,3 +5,17 @@ let bannerInfo = document.querySelector("#bannerInfo");
 bannerImage.addEventListener("click",event =>{
     bannerInfo.classList.toggle("bannerInfo");
 } );
+
+//LocalStorage for theme tooggle
+let themeToggle = document.querySelector("#themeToggle");
+
+themeToggle.addEventListener("click", event => {
+    let isDark = document.body.classList.toggle("dark-mode");
+
+    if (isDark) {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "🌙 Dark Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "☀️ Light Mode";
+    
