9fe241ced33e9d870b6088736cc139589c420757
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-07-03 08:41:26 +0300

    Booking Form validation

--- submission/js/script.js
+++ submission/js/script.js
@@ -44,3 +44,36 @@ if(item !== ""){
 
 }
 });
+
+//Booking Form Validation
+let bookingForm = document.querySelector("#bookingForm");
+let bookingFeedback = document.querySelector("#bookingFeedback");
+let bookingBtn = document.querySelector("#bookingBtn");
+
+bookingForm.addEventListener("submit", event=>{
+    event.preventDefault();
+    let name =document.querySelector("#name").value.trim();
+    let email =document.querySelector("#email").value.trim();
+    let phone = document.querySelector("#phone").value.trim();
+    let roomType =document.querySelector("#room_type").value;
+    let hostel =document.querySelector("#hostel").value;
+    let checkin = document.querySelector("#checkin").value;
+    let duration = document.querySelector("#duration_unit").value;
+
+    if(
+        name == ""||
+        email == ""||
+        phone == ""||
+        roomType == ""||
+        hostel == ""||
+        checkin == ""||
+        duration == ""
+    ){
+        bookingFeedback.textContent = "Please fill in all fields.";
+        bookingFeedback.style.color = "red";
+        return;
+    } 
+   bookingFeedback.textContent = "Booking successful !" +name+", your reservation at "+ hostel + " has been received.";
+   bookingFeedback.style.color = "green";
+   bookingForm.reset();
+});
